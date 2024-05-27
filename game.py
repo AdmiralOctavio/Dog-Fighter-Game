4b7f12af804b0e1d2d18e3d6b24492c9e1c53088
Flavio <flaviocicero55@gmail.com>
2024-05-27 23:26:05 +0200

    Update game.py

--- game.py
+++ game.py
@@ -3,7 +3,8 @@ import numpy
 import time
 
 pg.init()
-resolution = (1920,1080)
+width, height = 1920, 1080
+resolution = (width,height)
 screen = pg.display.set_mode(resolution)
 scrrect = screen.get_rect()
 black =(0, 0, 0)
@@ -13,23 +14,40 @@ ship = pg.image.load('Player.png')
 ship = pg.transform.scale(ship, (128, 96))
 enemy = pg.image.load('Enemy.png')
 enemy = pg.transform.scale(enemy, (128, 96))
-missile = pg.image.load('missile.png')
-missile = pg.transform.scale(missile, (128, 96))
+missileText = pg.image.load('missile.png')
+missileText = pg.transform.scale(missileText, (128, 96))
 
 shiprect = ship.get_rect()
 shiprect.centerx = 250
 enemyrect = enemy.get_rect()
 enemyrect.centerx = 500
-missilerect = missile.get_rect()
-
+missilerect = missileText.get_rect()
 shipDir = []
 enemyDir = []
 missileDir = []
-
+F_missilesArray = []
 speed = 3
-
+missileSpeed = 15
 running = True
-heading = 0
+F_heading = 0
+E_heading = 180
+
+class Missile:
+    def __init__(self, posx, posy, heading):
+        self.posx = posx
+        self.posy = posy
+        self.heading = heading
+    
+    def update(self):
+        self.posx += missileSpeed*numpy.cos(numpy.radians(self.heading))
+        self.posy -= missileSpeed*numpy.sin(numpy.radians(self.heading))
+
+    def render(self, screen):
+        missilerect = missileText.get_rect()
+        missilerect.centerx = self.posx
+        missilerect.centery = self.posy
+        screen.blit(missileText, missilerect)
+
 
 def turnLeft(heading):
     heading -= round(90/refreshRate)
@@ -45,34 +63,79 @@ def setup():
     for i in range(360):
         shipDir.append(pg.transform.rotate(ship, i))
         enemyDir.append(pg.transform.rotate(enemy, i))
-        missileDir.append(pg.transform.rotate(missile, i))
+        missileDir.append(pg.transform.rotate(missileText, i))
 
 setup()
-
-positionx = 540
-positiony = 540
+missilerect = missileText.get_rect()
+#Friendly Position
+F_positionx = width-500
+F_positiony = 540
+#Enemy Position
+E_positionx = 500
+E_positiony = 540
+enemy = enemyDir[180]
+#Main Game Loop
 while running:
     pg.event.pump()
     keys = pg.key.get_pressed()
-
+    event = pg.event.get()
+#Blue Player Rotation
     if keys[pg.K_a]:
-         heading = turnRight(heading)
+         F_heading = turnRight(F_heading)
          shiprect = ship.get_rect()
-         ship = shipDir[heading]
+         ship = shipDir[F_heading]
 
     elif keys[pg.K_d]:
-         heading = turnLeft(heading)
+         F_heading = turnLeft(F_heading)
          shiprect = ship.get_rect()
-         ship = shipDir[heading]
-    
-    positionx += speed*numpy.cos(numpy.radians(heading))
-    positiony -= speed*numpy.sin(numpy.radians(heading))
+         ship = shipDir[F_heading]
+#Red Player Rotation
+    if keys[pg.K_RIGHT]:
+         E_heading = turnRight(E_heading)
+         enemyrect = enemy.get_rect()
+         enemy = enemyDir[E_heading]
 
-    shiprect.centerx = positionx
-    shiprect.centery = positiony
+    elif keys[pg.K_LEFT]:
+         E_heading = turnLeft(E_heading)
+         enemyrect = enemy.get_rect()
+         enemy = enemyDir[E_heading]
+#Movement
+    F_positionx += speed*numpy.cos(numpy.radians(F_heading))
+    F_positiony -= speed*numpy.sin(numpy.radians(F_heading))
+
+    E_positionx += speed*numpy.cos(numpy.radians(E_heading))
+    E_positiony -= speed*numpy.sin(numpy.radians(E_heading))
+
+    shiprect.centerx = F_positionx
+    shiprect.centery = F_positiony
+
+    enemyrect.centerx = E_positionx
+    enemyrect.centery = E_positiony
+
+#Resets position when out of bounds
+    F_positionx %= width
+    F_positiony %= height
+    E_positionx %= width
+    E_positiony %= height
+
+#Missiles
+    if pg.key.get_pressed()[pg.K_SPACE]:
+        m = Missile(F_positionx, F_positiony, F_heading)
+        F_missilesArray.append(m)
+        print("balls")
+        time.sleep(0.5)
+    
+    for missile in F_missilesArray:
+        missile.render(screen)
+        missile.update()
+
+
+            
 
     pg.draw.rect(screen, black, scrrect)
     screen.blit(ship, shiprect)
+    screen.blit(enemy, enemyrect)
+
     pg.display.flip() 
     if keys[pg.K_TAB]:
             running = False    
