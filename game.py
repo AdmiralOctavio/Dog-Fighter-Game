647333dfb121a65e1b27eca15acf69d201eb4dda
Flavio <flaviocicero55@gmail.com>
2024-05-29 00:09:02 +0200

    Missile Destruction Animation

--- game.py
+++ game.py
@@ -26,28 +26,36 @@ shipDir = []
 enemyDir = []
 missileDir = []
 F_missilesArray = []
-speed = 3
-missileSpeed = 15
+E_missileArray = []
+explosions = []
+mwait = 0
+speed, missileSpeed = 3, 10
 running = True
-F_heading = 0
-E_heading = 180
+F_heading, E_heading = 0, 180
+isExploding = False
+timeafterexplosion = 1
+missiletemp = 0
+
+
 
 class Missile:
-    def __init__(self, posx, posy, heading):
+    def __init__(self, posx, posy, heading, timer):
         self.posx = posx
         self.posy = posy
         self.heading = heading
+        self.timer = timer
     
     def update(self):
         self.posx += missileSpeed*numpy.cos(numpy.radians(self.heading))
         self.posy -= missileSpeed*numpy.sin(numpy.radians(self.heading))
+        self.timer += 1
 
     def render(self, screen):
+        missileText = missileDir[self.heading]
         missilerect = missileText.get_rect()
         missilerect.centerx = self.posx
         missilerect.centery = self.posy
         screen.blit(missileText, missilerect)
-
 
 def turnLeft(heading):
     heading -= round(90/refreshRate)
@@ -64,15 +72,16 @@ def setup():
         shipDir.append(pg.transform.rotate(ship, i))
         enemyDir.append(pg.transform.rotate(enemy, i))
         missileDir.append(pg.transform.rotate(missileText, i))
+    for i in range(5):
+        explosion = pg.image.load('ex' + str(i+1) + '.png')
+        explosions.append(pg.transform.scale2x(explosion))
 
 setup()
 missilerect = missileText.get_rect()
 #Friendly Position
-F_positionx = width-500
-F_positiony = 540
+F_positionx, F_positiony = width-500, 540
 #Enemy Position
-E_positionx = 500
-E_positiony = 540
+E_positionx, E_positiony = 500, 540
 enemy = enemyDir[180]
 #Main Game Loop
 while running:
@@ -119,23 +128,46 @@ while running:
     E_positiony %= height
 
 #Missiles
-    if pg.key.get_pressed()[pg.K_SPACE]:
-        m = Missile(F_positionx, F_positiony, F_heading)
+    if pg.key.get_pressed()[pg.K_s] and mwait > 100:
+        mwait = 0
+        timer = 0
+        m = Missile(F_positionx, F_positiony, F_heading, timer)
         F_missilesArray.append(m)
-        print("balls")
-        time.sleep(0.5)
-    
-    for missile in F_missilesArray:
-        missile.render(screen)
-        missile.update()
-
-
-            
 
     pg.draw.rect(screen, black, scrrect)
     screen.blit(ship, shiprect)
     screen.blit(enemy, enemyrect)
 
+    for missile in F_missilesArray:
+        missile.render(screen)
+        missile.update()
+
+        if missile.timer > 100:
+            F_missilesArray.pop(0)
+            isExploding = True
+            missiletemp = 1
+            xpos = missile.posx
+            ypos = missile.posy
+#Missile Destruction Animation
+    if isExploding and missiletemp <5:
+        timeafterexplosion += 1
+        exp = explosions[missiletemp]
+        explosionrect = exp.get_rect()
+        explosionrect.centerx = xpos
+        explosionrect.centery = ypos
+        screen.blit(exp, explosionrect)
+
+    if timeafterexplosion % 16 == 0:
+        missiletemp += 1
+        
+    elif missiletemp == len(explosions):
+        timeafterexplosion = 1
+        isExploding = False
+        missiletemp = 0
+
+
+    print(missiletemp)
+    mwait += 1
     pg.display.flip() 
     if keys[pg.K_TAB]:
             running = False    
