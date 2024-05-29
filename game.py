d9f0b45907e51920adbad958ef8dbb85a6767986
Flavio <flaviocicero55@gmail.com>
2024-05-29 16:35:53 +0200

    Rocket Collision

--- game.py
+++ game.py
@@ -17,25 +17,21 @@ enemy = pg.transform.scale(enemy, (128, 96))
 missileText = pg.image.load('missile.png')
 missileText = pg.transform.scale(missileText, (128, 96))
 
-shiprect = ship.get_rect()
-shiprect.centerx = 250
-enemyrect = enemy.get_rect()
-enemyrect.centerx = 500
-missilerect = missileText.get_rect()
+shiprect, enemyrect, missilerect = ship.get_rect(), enemy.get_rect(), missileText.get_rect()
+shiprect.centerx, enemyrect.centerx = 250, 500
 shipDir = []
 enemyDir = []
 missileDir = []
 F_missilesArray = []
 E_missilesArray = []
 explosions = []
-Emwait = 0
-Fmwait = 0
+Emwait, Fmwait = 0, 0
 speed, missileSpeed = 3, 10
+Fscore, Escore = 0, 0
 running = True
 F_heading, E_heading = 0, 180
 isExploding = False
-timeafterexplosion = 1
-missiletemp = 0
+timeafterexplosion, missiletemp = 1, 0
 
 class Missile:
     def __init__(self, posx, posy, heading, timer, origin):
@@ -53,8 +49,7 @@ class Missile:
     def render(self, screen):
         missileText = missileDir[self.heading]
         missilerect = missileText.get_rect()
-        missilerect.centerx = self.posx
-        missilerect.centery = self.posy
+        missilerect.centerx, missilerect.centery = self.posx, self.posy
         screen.blit(missileText, missilerect)
 
 def turnLeft(heading):
@@ -115,11 +110,8 @@ while running:
     E_positionx += speed*numpy.cos(numpy.radians(E_heading))
     E_positiony -= speed*numpy.sin(numpy.radians(E_heading))
 
-    shiprect.centerx = F_positionx
-    shiprect.centery = F_positiony
-
-    enemyrect.centerx = E_positionx
-    enemyrect.centery = E_positiony
+    shiprect.centerx, shiprect.centery = F_positionx, F_positiony
+    enemyrect.centerx, enemyrect.centery = E_positionx, E_positiony
 
 #Resets position when out of bounds
     F_positionx %= width
@@ -129,60 +121,65 @@ while running:
 
 #Missiles
     if pg.key.get_pressed()[pg.K_s] and Fmwait > 100: #Friendly Missile Launch
-        Fmwait = 0
-        Ftimer = 0
+        Fmwait, Ftimer = 0, 0
         m = Missile(F_positionx, F_positiony, F_heading, Ftimer, "Blue")
         F_missilesArray.append(m)
 
     if pg.key.get_pressed()[pg.K_DOWN] and Emwait > 100: #Enemy Missile Launch
-        Emwait = 0
-        Etimer = 0
+        Emwait, Etimer = 0, 0
         m = Missile(E_positionx, E_positiony, E_heading, Etimer, "Red")
         E_missilesArray.append(m)
 
     pg.draw.rect(screen, black, scrrect)
     screen.blit(ship, shiprect)
     screen.blit(enemy, enemyrect)
-
+#Looping through Friendly missile array 
     for missile in F_missilesArray:
         missile.render(screen)
         missile.update()
 
         if missile.timer > 100:
             F_missilesArray.pop(0)
-            isExploding = True
-            missiletemp = 1
-            xpos = missile.posx
-            ypos = missile.posy
+            isExploding, missiletemp = True, 1
+            xpos, ypos = missile.posx, missile.posy
+        
+        if abs(E_positionx - missile.posx) < 40 and abs(E_positiony - missile.posy) < 40:
+            missile.timer = 100
+            EisDead = True
 
+        
+#Looping through Enemy missile array
     for missile in E_missilesArray:
         missile.render(screen)
         missile.update()
 
         if missile.timer > 100:
             E_missilesArray.pop(0)
-            isExploding = True
-            missiletemp = 1
-            xpos = missile.posx
-            ypos = missile.posy
+            isExploding, missiletemp = True, 1
+            xpos, ypos = missile.posx, missile.posy
+
+        if abs(F_positionx - missile.posx) < 40 and abs(F_positiony - missile.posy) < 40:
+            missile.timer = 100
+            FisDead = True
+
+    if FisDead == True:
+        Escore += 1
+        FisDead = False
+
 #Missile Destruction Animation
     if isExploding and missiletemp <5:
         timeafterexplosion += 1
         exp = explosions[missiletemp]
         explosionrect = exp.get_rect()
-        explosionrect.centerx = xpos
-        explosionrect.centery = ypos
+        explosionrect.centerx, explosionrect.centery = xpos, ypos
         screen.blit(exp, explosionrect)
 
     if timeafterexplosion % 16 == 0:
         missiletemp += 1
         
     elif missiletemp == len(explosions):
-        timeafterexplosion = 1
-        isExploding = False
-        missiletemp = 0
+        timeafterexplosion, missiletemp, isExploding = 1, 0, False
 
-    print(missiletemp)
     Emwait += 1
     Fmwait += 1
     pg.display.flip() 
